0c3c6852dfaf908baf5d39439ecc989a4eee2ce6
Marco Steinhoff <steinhoff@locity.de>
2024-07-11 12:50:53 +0200

    Add fav icon

--- apps/locity-de/app/layout.tsx
+++ apps/locity-de/app/layout.tsx
@@ -12,7 +12,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Locity GmbH</title>
+        <base href="/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/fav/icon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/fav/icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/fav/icon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/fav/icon-16x16.png" />
+        <link rel="manifest" href="/fav/manifest.json" />
+        <link rel="mask-icon" href="/fav/icon-mono.svg" color="#5bbad5" />
+        <meta name="msapplication-config" content="fav/browserconfig.xml" />
+        <meta name="theme-color" content="#4671D5" />
+      </head>
     </html>
   );
 }
