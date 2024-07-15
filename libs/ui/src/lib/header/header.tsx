25a96d37738bd2321db50aa34147b85189f0cd16
Marco Steinhoff <steinhoff@locity.de>
2024-07-15 14:41:18 +0200

    Add font, some styling und updates

--- libs/ui/src/lib/header/header.tsx
+++ libs/ui/src/lib/header/header.tsx
@@ -4,6 +4,7 @@ import styles from './header.module.scss'
 export function Header() {
   return (
     <div className={styles['container']}>
+      <img className={styles['logo']} src="./locity.svg" alt="Locity Logo" />
       <h1>Locity</h1>
       <Nav />
     </div>
